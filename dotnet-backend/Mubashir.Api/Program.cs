71f911dd64a8a5d8bbbd743e0ce18de80d46856c
Mubashir Hasan <mubashir105@gmail.com>
2026-02-14 16:58:55 +0500

    Fix: Simplified Swagger to resolve namespace error

--- dotnet-backend/Mubashir.Api/Program.cs
+++ dotnet-backend/Mubashir.Api/Program.cs
@@ -1,25 +1,23 @@
 using Npgsql;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Swagger Services add karein
+// Basic Swagger Setup
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mubashir Platform API", Version = "v1" });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger Middleware
+// Swagger Middleware (Always on for now so we can test)
 app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mubashir API v1"));
+app.UseSwaggerUI();
 
+// Database Connection String
 string connectionString = "Host=mubashir-db;Database=postgres;Username=postgres;Password=mubashir.105";
 
-app.MapGet("/", () => "Mubashir .NET 9 API is Running!");
+app.MapGet("/", () => "Mubashir .NET 9 API is Live and Running!");
 
+// API to get news from Database
 app.MapGet("/api/banking-news", async () =>
 {
     var newsList = new List<object>();
